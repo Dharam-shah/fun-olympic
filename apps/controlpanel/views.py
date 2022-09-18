b45c9676a08dbc479ef97a160cfab67e251c2545
Dharam-shah <dharmendra11250@gmail.com>
2022-09-18 21:48:39 +0545

    created model for game,game-categogy and fixed table style

--- apps/controlpanel/views.py
+++ apps/controlpanel/views.py
@@ -3,3 +3,6 @@ from django.views.generic import TemplateView
 # Create your views here.
 class DashboardOverView(TemplateView):
     template_name = 'controlpanel/base.html'
+
+class CategoryListView(TemplateView):
+    template_name = 'controlpanel/category/category_list.html'
