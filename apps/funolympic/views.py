85c4643a032b87086faf2ff4392e8342d2fdd15c
Dharam-shah <dharmendra11250@gmail.com>
2022-09-21 21:40:07 +0545

    setup url for user profilr page

--- apps/funolympic/views.py
+++ apps/funolympic/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, "homepage/homepage.html")
+
+
+def user_profile(request):
+    return render(request, "user/user_profile.html")
