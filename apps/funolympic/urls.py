1ce14eeb0f602a501bf54adc5e9b1e0f9eb96f5c
Dharam-shah <dharmendra11250@gmail.com>
2022-09-22 23:06:31 +0545

    homepage dynamic completed

--- apps/funolympic/urls.py
+++ apps/funolympic/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import home, user_profile
+from .views import OnlympicGameView, CategoryDetailView, user_profile
 
 urlpatterns = [
-    path('', home, name='homepage'),
+    # path('', home, name='homepage'),
     path('profile/', user_profile, name='user-profile'),
+
+    path("", OnlympicGameView.as_view(), name="homepage"),
+    path("game-category/", CategoryDetailView.as_view(), name="game-category"),
 ]
--- apps/funolympic/views.py
+++ apps/funolympic/views.py
@@ -1,8 +1,23 @@
+from django.views.generic import ListView
+from apps.funolympic.models import Category, OlympicGame 
 from django.shortcuts import render
 
+
 # Create your views here.
-def home(request):
-    return render(request, "homepage/homepage.html")
+class OnlympicGameView(ListView):
+    model = OlympicGame
+    context_object_name = 'game_lists'
+    template_name = 'homepage/homepage.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['categories'] = Category.objects.all()
+        return context
+
+class CategoryDetailView(ListView):
+    model = Category
+    context_object_name = 'category_lists'
+    template_name = 'homepage/game_category.html'
 
 
 def user_profile(request):
