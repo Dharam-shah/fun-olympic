0942f117fcc611ac5a955b7037785f705a9865d8
Gajendra <gajendrasah111@gmail.com>
2022-09-25 20:29:58 +0545

    Back to Top function added

--- static/js/main.js
+++ static/js/main.js
@@ -70,6 +70,25 @@
             ],
         });
 
+        var btn = $('.back-to-top');
+
+        $(window).scroll(function () {
+            if ($(window).scrollTop() > 150) {
+                btn.addClass('show');
+            } else {
+                btn.removeClass('show');
+            }
+        });
+
+        btn.on('click', function (e) {
+            e.preventDefault();
+            $('html, body').animate({
+                scrollTop: 0
+            }, '500');
+        });
+
+
+
         let loadMoreBtn = document.querySelector('#load-more');
         let currentItem = 4;
 
